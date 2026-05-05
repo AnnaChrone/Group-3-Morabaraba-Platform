a11bbf0c0a318a0429c075b7367208195373d4be
tokidokiyukiii <2658083@students.wits.ac.za>
2026-05-05 15:43:52 +0200

    LOBBY username syncing

---
+++ Assets/Scripts/PlayerData.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class PlayerData : MonoBehaviour
+{
+    public static PlayerData Instance { get; private set; }
+
+    public string Username { get; private set; } = "Guest";
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            Debug.Log("✅ PlayerData initialized");
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetUsername(string username)
+    {
+        if (string.IsNullOrEmpty(username))
+        {
+            Username = "Guest";
+        }
+        else
+        {
+            Username = username.Trim();
+            if (Username.Length > 20) Username = Username.Substring(0, 20);
+        }
+        Debug.Log($"📝 PlayerData.Username set to: '{Username}'");
+    }
+
+    public void ClearUsername()
+    {
+        Username = "Guest";
+    }
+}
